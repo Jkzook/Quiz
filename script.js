9bac743908dfe342d2b3a767f1c280a4fcc629f2
Jkzook <119649225+Jkzook@users.noreply.github.com>
2023-01-25 19:22:05 -0700

    mainheader

---
+++ script.js
@@ -0,0 +1,7 @@
+let questions = {number: 1,
+quesion: "What does vanilla JavaScript mean?",
+answer: "simple, less complex JavaScript",
+options: "ice cream flavor",
+          "complex JavaScript",
+          "an extension for VScode"
+        }

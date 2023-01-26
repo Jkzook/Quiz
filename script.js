2707f8b859eb5352d22e3eb3c22129944244f6e5
Jkzook <119649225+Jkzook@users.noreply.github.com>
2023-01-26 16:11:55 -0700

    javaQuestions

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-let questions = {number: 1,
-quesion: "What does vanilla JavaScript mean?",
-answer: "simple, less complex JavaScript",
-options: "ice cream flavor",
-          "complex JavaScript",
-          "an extension for VScode"
-        }
+const startBtn = document.getElementById('start');
+const previousBtn = document.getElementById('previous');
+const nextBtn = document.getElementById('next');
+const submitBtn = .getElementById('submit');
+const studentScoreBtn = document.getElementById('score');
+const questionTText = document.getElementById('text');
+
+let currentQuestion = 0;
+var score = 0;
+
+let questions = [
+        {question 1: "what is vanilla JavaScript?",
+                  answers: [
+                    {option: "ice cream  flavor",answer:wrong},
+                    {option: "basic JavaScript",answer:right},
+                    {option: "a type of html",answer:wrong},
+                
+                ]}]
+        {question 2: "what does html stand for?",
+                answers: [
+                  {option: "how to make langauge",answer:wrong},
+                  {option: "the style sheet for a website",answer:wrong},
+                  {option: "hypertext markup language",answer:right},
+              
+              ]}]
+    
+
+
+
+
+
+
+[
+     question: what is vanilla JavaScript?,
+     answers:
+         [{option a: "ice cream flavor"}
+          { option b: "basic JavaScript"}
+          {option c: "a type of html"}
+]
